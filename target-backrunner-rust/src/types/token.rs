459b0bbc77414d31c284d5d97936094577d820ce
mbecker20 <becker.maxh@gmail.com>
2021-07-11 12:54:19 -0700

    take amount and coin decimals

--- target-backrunner-rust/src/types/token.rs
+++ target-backrunner-rust/src/types/token.rs
@@ -1,21 +1,27 @@
-use ethabi::ethereum_types::U256;
-use primitive_types::U256 as PrimU256;
+use primitive_types::U256;
 use std::{cmp::min, str::FromStr};
 use web3::types::H160;
 
 pub struct Token {
     pub address: H160,
-    min_trade_amount_wei: PrimU256,
+    min_trade_amount_wei: U256,
 }
 impl Token {
-    pub fn new(address: &str, min_trade_amount_wei: &str) -> Token {
+    // pub fn new(address: &str, min_trade_amount_wei: &str) -> Token {
+    //     Token {
+    //         address: H160::from_str(address).unwrap(),
+    //         min_trade_amount_wei: PrimU256::from_dec_str(min_trade_amount_wei).unwrap(),
+    //     }
+    // }
+
+    pub fn new(address: &str, min_trade_amount: usize, decimals: usize) -> Token {
         Token {
             address: H160::from_str(address).unwrap(),
-            min_trade_amount_wei: PrimU256::from_dec_str(min_trade_amount_wei).unwrap(),
+            min_trade_amount_wei: U256::from(min_trade_amount).saturating_mul(U256::exp10(decimals)),
         }
     }
 
-    pub fn above_trade_threshold(&self, amount: &PrimU256) -> bool {
+    pub fn above_trade_threshold(&self, amount: &U256) -> bool {
         amount.gt(&self.min_trade_amount_wei)
     }
 }
