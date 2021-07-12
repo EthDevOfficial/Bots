24f2b3dc57f906ed9c03ab4d0e15a612307f8fe6
mbecker20 <becker.maxh@gmail.com>
2021-07-12 15:25:13 -0700

    loading print statements

--- target-backrunner-rust/src/main.rs
+++ target-backrunner-rust/src/main.rs
@@ -10,10 +10,11 @@ use web3::futures::StreamExt;
 #[tokio::main]
 async fn main() {
     println!("starting target backrunner");
-
     let immutable_state = loaders::load_immutable_state().await;
+    println!("loaded immutable state");
     let mutable_state = loaders::load_mutable_state(&immutable_state).await;
-    println!("starting target backrunner");
+    println!("loaded mutable state");
+    println!("target backrunner started");
 
     immutable_state
         .web3
