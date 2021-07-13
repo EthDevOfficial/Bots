39ab0c785e823c34b865089c1b826d846510153c
Dara Shahriari <Dara.shahriari@mail.mcgill.ca>
2021-07-13 10:04:55 -0400

    adjusted min trade amounts

--- target-backrunner-rust/src/loaders/polygon.rs
+++ target-backrunner-rust/src/loaders/polygon.rs
@@ -56,19 +56,19 @@ struct Tokens {
 impl Tokens {
     pub fn new() -> Tokens {
         Tokens {
-            wmatic: Token::new("0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", 100, 1, 18),
-            weth: Token::new("0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 5, 100, 18),
-            pwings: Token::new("0x845e76a8691423fbc4ecb8dd77556cb61c09ee25", 1000, 1, 18),
-            quick: Token::new("0x831753DD7087CaC61aB5644b308642cc1c33Dc13", 3, 10, 18),
-            wbtc: Token::new("0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6", 3, 1000, 8),
-            usdt: Token::new("0xc2132D05D31c914a87C6611C10748AEb04B58e8F", 100, 1, 6),
+            wmatic: Token::new("0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", 1000, 1, 18),
+            weth: Token::new("0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 5, 10, 18),
+            pwings: Token::new("0x845e76a8691423fbc4ecb8dd77556cb61c09ee25", 10000, 1, 18),
+            quick: Token::new("0x831753DD7087CaC61aB5644b308642cc1c33Dc13", 3, 1, 18),
+            wbtc: Token::new("0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6", 3, 100, 8),
+            usdt: Token::new("0xc2132D05D31c914a87C6611C10748AEb04B58e8F", 1000, 1, 6),
             dai: Token::new("0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", 0, 1, 18),
-            usdc: Token::new("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", 100, 1, 6),
-            pup: Token::new("0xcFe2cF35D2bDDE84967e67d00aD74237e234CE59", 2, 1, 18),
-            dfyn: Token::new("0xC168E40227E4ebD8C1caE80F7a55a4F0e6D66C97", 50, 1, 18),
-            bone: Token::new("0x6bb45cEAC714c52342Ef73ec663479da35934bf7", 2, 1, 18),
-            route: Token::new("0x16ECCfDbb4eE1A85A33f3A9B21175Cd7Ae753dB4", 0, 1, 18),
-            titan: Token::new("0xaAa5B9e6c589642f98a1cDA99B9D024B8407285A", 1000000, 1, 18),
+            usdc: Token::new("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", 1000, 1, 6),
+            pup: Token::new("0xcFe2cF35D2bDDE84967e67d00aD74237e234CE59", 20, 1, 18),
+            dfyn: Token::new("0xC168E40227E4ebD8C1caE80F7a55a4F0e6D66C97", 500, 1, 18),
+            bone: Token::new("0x6bb45cEAC714c52342Ef73ec663479da35934bf7", 20, 1, 18),
+            route: Token::new("0x16ECCfDbb4eE1A85A33f3A9B21175Cd7Ae753dB4", 100, 1, 18),
+            titan: Token::new("0xaAa5B9e6c589642f98a1cDA99B9D024B8407285A", 100000000, 1, 18),
         }
     }
 }
