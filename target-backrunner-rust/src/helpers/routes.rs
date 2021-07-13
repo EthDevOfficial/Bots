6493535e985f9997a4f9524519bf553e18249a6d
mbecker20 <becker.maxh@gmail.com>
2021-07-13 09:31:47 -0700

    send::routes

--- target-backrunner-rust/src/helpers/routes.rs
+++ target-backrunner-rust/src/helpers/routes.rs
@@ -72,22 +72,14 @@ pub async fn make_simple_routes(
         }
     };
 
-    for i in (0..routes.len()).step_by(immutable_state.bundle_size) {
-        let (tx_obj, wallet_index) = make_simple_tx(
-            immutable_state,
-            Vec::from(&routes[i..min(i + immutable_state.bundle_size, routes.len())]),
-            mutable_state,
-            gas_price,
-        );
-        send_transaction(immutable_state, mutable_state, wallet_index, tx_obj).await;
-    }
-
-    // while routes.len() > 0 {
-    //     let bundle: Vec<Bytes> = routes.drain(0..min(bundle_size, routes.len())).collect();
-    //     let (tx_obj, wallet_index) =
-    //         make_simple_tx(immutable_state, bundle, mutable_state, gas_price);
-    //     send_transaction(immutable_state, mutable_state, wallet_index, tx_obj).await;
-    // }
+    send_routes(
+        routes,
+        gas_price,
+        make_simple_tx,
+        immutable_state,
+        mutable_state,
+    )
+    .await;
 }
 
 pub async fn make_outer_tri_routes(
@@ -141,22 +133,14 @@ pub async fn make_outer_tri_routes(
         }
     }
 
-    for i in (0..routes.len()).step_by(immutable_state.bundle_size) {
-        let (tx_obj, wallet_index) = make_tri_tx(
-            immutable_state,
-            Vec::from(&routes[i..min(i + immutable_state.bundle_size, routes.len())]),
-            mutable_state,
-            gas_price,
-        );
-        send_transaction(immutable_state, mutable_state, wallet_index, tx_obj).await;
-    }
-
-    // let bundle_size = immutable_state.bundle_size;
-    // while routes.len() > 0 {
-    //     let bundle: Vec<Bytes> = routes.drain(0..min(bundle_size, routes.len())).collect();
-    //     let (tx_obj, wallet_index) = make_tri_tx(immutable_state, bundle, mutable_state, gas_price);
-    //     send_transaction(immutable_state, mutable_state, wallet_index, tx_obj).await;
-    // }
+    send_routes(
+        routes,
+        gas_price,
+        make_tri_tx,
+        immutable_state,
+        mutable_state,
+    )
+    .await;
 }
 
 pub async fn make_inner_tri_routes(
