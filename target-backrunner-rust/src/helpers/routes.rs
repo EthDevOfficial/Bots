2993f2c09b60460b1f20378997b13e392811a287
paulvaden <pbdonut@gmail.com>
2021-07-13 18:33:09 -0400

    printing funcs

--- target-backrunner-rust/src/helpers/routes.rs
+++ target-backrunner-rust/src/helpers/routes.rs
@@ -11,6 +11,62 @@ use std::sync::Arc;
 use std::vec::Vec;
 use web3::types::{TransactionParameters, H160};
 
+pub async fn make_simple_routes_firebird(
+    token1: &H160,
+    token2: &H160,
+    gas_price: U256,
+    immutable_state: &Arc<ImmutableState>,
+    mutable_state: &Arc<MutableState>,
+) {
+    let mut routes: Vec<Bytes> = Vec::new();
+
+    // Find the correct pool ordering and record whether the route should be reversed
+    let (token1, token2, should_reverse) = favor_outer_token(token1, token2, &immutable_state);
+
+    // Make the routes
+    // TODO: seperate into 4 blocks
+    if should_reverse {
+        for i in 0..immutable_state.exchanges.len() {
+            for j in i+1..immutable_state.exchanges.len(){
+                let away_exchange = &immutable_state.exchanges[i];
+                let return_exchange = &immutable_state.exchanges[j];
+                // (T1, T2, E2) -> (T2, T1, E1)
+                routes.push(tokenize_simple(
+                    token1,
+                    token2,
+                    &away_exchange.router,
+                    &return_exchange.router,
+                    U256::from(away_exchange.swap_fee + return_exchange.swap_fee),
+                    ));
+            }
+        }
+    } else {
+        for i in 0..immutable_state.exchanges.len() {
+            for j in i+1..immutable_state.exchanges.len(){
+                let away_exchange = &immutable_state.exchanges[j];
+                let return_exchange = &immutable_state.exchanges[i];
+                // (T1, T2, E1) -> (T2, T1. E2)
+                routes.push(tokenize_simple(
+                    token1,
+                    token2,
+                    &away_exchange.router,
+                    &return_exchange.router,
+                    U256::from(away_exchange.swap_fee + return_exchange.swap_fee),
+                ));
+            }
+        }
+    };
+
+    send_routes(
+        routes,
+        gas_price,
+        make_simple_tx,
+        immutable_state,
+        mutable_state,
+    )
+    .await;
+}
+
 pub async fn make_simple_routes(
     token1: &H160,
     token2: &H160,
@@ -82,6 +138,63 @@ pub async fn make_simple_routes(
     .await;
 }
 
+
+pub async fn make_outer_tri_routes_firebird(
+    token1: &H160,
+    token2: &H160,
+    gas_price: U256,
+    immutable_state: &Arc<ImmutableState>,
+    mutable_state: &Arc<MutableState>,
+) {
+    // Find the correct pool ordering and record whether the route should be reversed
+    let (token1, token2, should_reverse) = favor_outer_token(token1, token2, &immutable_state);
+
+    // Token1 is preferred
+    let mut routes: Vec<Bytes> = Vec::new();
+    for other_exchange in immutable_state.exchanges.iter() {
+        for inner_token in immutable_state.inner_tokens.iter() {
+            if token1.ne(&inner_token.address) && token2.ne(&inner_token.address) {
+                if should_reverse {
+                    // (T1, InnerT, E3) -> (InnerT, T2, E2) -> (T2, T1, E1)
+                    routes.push(tokenize_tri(
+                        token1,
+                        &inner_token.address,
+                        token2,
+                        &other_exchange.router,
+                        &other_exchange.router,
+                        &other_exchange.router,
+                        U256::from(
+                            other_exchange.swap_fee * 3
+                        ),
+                    ));
+                } else {
+                    // (T1, T2, E1) -> (T2, InnerT, E2) -> (InnerT, T1, E3)
+                    routes.push(tokenize_tri(
+                        token1,
+                        token2,
+                        &inner_token.address,
+                        &other_exchange.router,
+                        &other_exchange.router,
+                        &other_exchange.router,
+                        U256::from(
+                            other_exchange.swap_fee * 3,
+                        ),
+                    ));
+                }
+            }
+        }
+    }
+
+    send_routes(
+        routes,
+        gas_price,
+        make_tri_tx,
+        immutable_state,
+        mutable_state,
+    )
+    .await;
+}
+
 pub async fn make_outer_tri_routes(
     token1: &H160,
     token2: &H160,
@@ -142,6 +255,46 @@ pub async fn make_outer_tri_routes(
     )
     .await;
 }
+
+pub async fn make_inner_tri_routes_firebird(
+    token1: &H160,
+    token2: &H160,
+    gas_price: U256,
+    immutable_state: &Arc<ImmutableState>,
+    mutable_state: &Arc<MutableState>,
+) {
+    // In original ordering (no outer token)
+    let mut routes: Vec<Bytes> = Vec::new();
+    for other_exchange in immutable_state.exchanges.iter() {
+        for outer_token in immutable_state.outer_tokens.iter() {
+            if token1.ne(&outer_token.address) && token2.ne(&outer_token.address) {
+                // We want to reverse
+                // (OuterT, T2 E3) -> (T2, T1, E2) -> (T1, OuterT, E1)
+                routes.push(tokenize_tri(
+                    &outer_token.address,
+                    token2,
+                    token1,
+                    &other_exchange.router,
+                    &other_exchange.router,
+                    &other_exchange.router,
+                    U256::from(
+                        other_exchange.swap_fee * 3,
+                    ),
+                ));
+            }
+        }
+    }
+
+    send_routes(
+        routes,
+        gas_price,
+        make_tri_tx,
+        immutable_state,
+        mutable_state,
+    )
+    .await;
+}
+
 
 pub async fn make_inner_tri_routes(
     token1: &H160,
